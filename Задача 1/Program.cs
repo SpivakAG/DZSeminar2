6d56913a927e467b8d38654b478cb1d76ce68b0c
ArtemSpivak <spivak25@yandex.ru>
2022-04-14 22:37:16 +0300

    задача решена

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int SecondSymbolEjector(int NumberInput)
+{
+    int firstAndSecond = NumberInput / 10;
+    int result = firstAndSecond % 10;
+    return result;
+}
+int result = SecondSymbolEjector(number);
+
+Console.WriteLine(result);
