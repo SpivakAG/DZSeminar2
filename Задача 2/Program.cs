77a771623089af5b84a575dd9f4f50620fa7dcb7
ArtemSpivak <spivak25@yandex.ru>
2022-04-14 23:30:53 +0300

    Задача решена

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,14 +1,33 @@
-﻿//Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифруэтого числа.
-int number = new Random().Next(100, 1000);
-Console.WriteLine(number);
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
 
-int Result(int input)
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+ {
+     Console.WriteLine("Третьей цифры нет");
+}
+else
 {
-    int first = input / 100;
-    int third = input % 10;
-    int result = first *10 + third;
+    int reversenumber = ReverseNumber(number);
+    int result = ThirdNumber(reversenumber);
+    Console.WriteLine(result);
+}
+int ReverseNumber (int n1)
+{
+    int result = 0;
+    while (n1 > 0)
+    {
+    result *= 10;
+    result += n1 % 10;
+    n1 /= 10;
+    }
     return result;
 }
 
-int result = Result (number);
-Console.WriteLine(result);
+int ThirdNumber (int number)
+{
+    number %= 1000;
+    number /= 100;
+    return number;
+}
