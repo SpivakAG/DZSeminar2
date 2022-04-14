0aa9a394cd1541d33d986e32da3362dd941c76c1
ArtemSpivak <spivak25@yandex.ru>
2022-04-14 23:55:38 +0300

    Задача решена без усложнения

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,3 +1,21 @@
 ﻿//Доп. задача: Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше
 //Доп. задача с усложнением: на отгадывание дается 3 попытки.
 
+int number = new Random().Next(1,10);
+Console.WriteLine(number);
+
+Console.WriteLine("Введите число от 1 до 10:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber == number)
+{
+    Console.WriteLine("Вы угадали!!!");
+}
+else
+{
+    if (userNumber > number)
+    {
+        Console.WriteLine("Загаданое число меньше вашего :'(");
+    }
+    else Console.WriteLine("Загаданое число больше вашего :'(");
+}
