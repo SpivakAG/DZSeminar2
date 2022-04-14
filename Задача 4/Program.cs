9911f92141cf6b693ab9abfacc20772328bb8dfa
ArtemSpivak <spivak25@yandex.ru>
2022-04-15 00:12:01 +0300

    задача верно решена

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -4,18 +4,27 @@
 int number = new Random().Next(1,10);
 Console.WriteLine(number);
 
-Console.WriteLine("Введите число от 1 до 10:");
-int userNumber = Convert.ToInt32(Console.ReadLine());
+int userNumber = Input(number);
 
-if (userNumber == number)
-{
-    Console.WriteLine("Вы угадали!!!");
-}
-else
+while (userNumber != number)
 {
     if (userNumber > number)
     {
         Console.WriteLine("Загаданое число меньше вашего :'(");
+        userNumber = Input(number);
     }
-    else Console.WriteLine("Загаданое число больше вашего :'(");
+    else 
+    {
+        Console.WriteLine("Загаданое число больше вашего :'(");
+        userNumber = Input(number);
+    }
+}
+    Console.WriteLine("Вы угадали!!!");
+    
+
+int Input (int n1)
+{
+Console.WriteLine("Введите число от 1 до 10:");
+int InputUserNumber = Convert.ToInt32(Console.ReadLine());
+return InputUserNumber;
 }
