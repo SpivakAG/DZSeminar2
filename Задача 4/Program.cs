f277d6bb5bd5eafdbf99a6799bb588561b3060d1
Artem Spivak <spivak25@yandex.ru>
2022-04-19 20:56:57 +0300

    Update Program.cs

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,4 +1,4 @@
-﻿//Доп. задача: Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше
+//Доп. задача: Игра угадайка. Программа загадывает случайное число. Пользователь его угадывает. Если пользователь дает неправильный ответ, то программа сообщает, больше загаданное число или меньше
 //Доп. задача с усложнением: на отгадывание дается 3 попытки.
 
 int number = new Random().Next(1,10);
@@ -6,7 +6,7 @@ Console.WriteLine(number);
 
 int wrongAnswers = 0; 
 
-int userNumber = Input(number);
+int userNumber = Input();
 
 while (userNumber != number)  
 {
@@ -16,13 +16,13 @@ while (userNumber != number)
         {
             Console.WriteLine("Загаданое число меньше вашего :'(");
             wrongAnswers +=1;
-            userNumber = Input(number);
+            userNumber = Input();
         }
         else 
         {
             Console.WriteLine("Загаданое число больше вашего :'(");
             wrongAnswers +=1;
-            userNumber = Input(number);
+            userNumber = Input();
         }
     }
     else break;  // в случае если попыток нет, цикл прекращается
@@ -33,9 +33,8 @@ if (userNumber == number)  Console.WriteLine("Вы угадали!!!");
 else Console.WriteLine("Вы проиграли");
     
 
-int Input (int n1) // функция ввода данных пользователем
+int Input () // функция ввода данных пользователем
 {
 Console.WriteLine("Введите число от 1 до 10:");
-int InputUserNumber = Convert.ToInt32(Console.ReadLine());
-return InputUserNumber;
+return Convert.ToInt32(Console.ReadLine());
 }
